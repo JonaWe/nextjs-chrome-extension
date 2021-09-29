f1c5905f4b032c1e8e2da9685c783eea555a9c2f
Jona Wessendorf <jonawessendorf@gmail.com>
2021-09-29 19:59:36 +0200

    new tab works again

--- pages/newtab.tsx
+++ pages/newtab.tsx
@@ -1,14 +1,44 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import styled from 'styled-components';
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+
+  object-fit: cover;
+
+  opacity: 0;
+  animation: fadeIn ease-in 1;
+  animation-fill-mode: forwards;
+  animation-duration: 0.15s;
+  animation-delay: 0.2s;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: none;
+  background-color: black;
+`;
 
 const Page: NextPage = () => {
   return (
-    <div>
+    <Wrapper>
       <Head>
-        <title>Create Next App</title>
+        <title>New Tab</title>
       </Head>
-      <h1>New Tab</h1>
-    </div>
+      <Img src="images/newtab.jpg" />
+    </Wrapper>
   );
 };
 
